b74bd2216efa151aff696178d0ba0a3bde0424c6
Dylan Park <dylpark@protonmail.com>
2021-10-28 17:35:35 +1000

    Delete Avatar.jsx

--- birthday-reminders/src/components/Avatar.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./App.css";
-
-function Avatar(props) {
-  return <img className="Circle-avatar" src={props.img} alt="avatar_img" />;
-}
-
-export default Avatar;
